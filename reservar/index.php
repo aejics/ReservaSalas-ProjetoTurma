e2094e931d2650dba023882ace2af6f01bb6a76f
Marco Pisco <a11531@aejics.org>
2025-03-18 12:15:43 +0000

    task: adicionar bootstrap corrigir action do form

--- reservar/index.php
+++ reservar/index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservar uma Sala</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Reservar uma Sala</h1>
-    <form action="/reservar.php">
+    <form method="POST" action="/reservar/criarreserva">
         <label for="sala">Sala:</label>
         <input type="text" name="sala" id="sala">
         <br>
