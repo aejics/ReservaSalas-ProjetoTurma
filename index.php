4149a88c4e83b01ab67ada8e310b9b36f0269d0f
Tomás Almeida <a9992@aejics.org>
2025-03-19 18:15:46 +0000

    ta perfeito nao mudar por enquanto

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
 </head>
-<<<<<<< HEAD
 <body>
 <div class="container">
 <div class="content">
@@ -38,9 +37,6 @@
 </body>
 </html>
 <body class="center">
-    <br>
-    <h1>Pagina Inicial</h1>
-    <h1>Esta é a Pagina Inicial para Reservar a Sala Multimédia</h1>
 <br>
 <br>
 <br>
@@ -65,7 +61,6 @@
 <br>
 <br>
 <br>
-    <h1>Esta é a Pagina Inicial para Reservar a Sala Multimédia</h1>
 <br>
 <br>
 <br>
@@ -89,12 +84,7 @@
 <br>
 <br>
 <br>
+<br>  
 <br>
-    <br>
+<br>
 <h2>
-Este projeto é possível graças aos trabalhos das seguintes turmas:
-<br>
-1ºD - Design de Funcionalidades Possíveis para a Aplicação
-2ºE - Criação das Expressões para o Backend SQL, e criação da aplicação.
-</h2>
->>>>>>> 3b0ce86b8819642b0fd3f09f47baf4283f8e1b53
