617842bbb6a6b5f90cbbefa4e7fc143b679be186
Tomás Almeida <a9992@aejics.org>
2025-03-25 11:39:09 +0000

    mudança de codigos e de css

--- index.php
+++ index.php
@@ -1,92 +1,71 @@
-<!DOCTYPE html>
 <html>
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+}
+.container {
+    text-align: center;
+    padding: 20px;
+}
+.icon-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+}
+.icon-button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+}
+.icon-button i {
+    font-size: 24px;
+    color: #333;
+    padding: 10px;
+    border-radius: 50%;
+    transition: background-color 0.3s ease, color 0.3s ease;
+}
+.icon-button:hover i {
+    background-color: rgba(0,0,0,0.1);
+    transform: scale(1.2);
+}
+.icon-button:active i {
+    background-color: rgba(0,0,0,0.2);
+}
+</style>
 </head>
 <body>
 <div class="container">
-<div class="content">
+<di class="content">
 <h1>Reserva Salas</h1>
-<p></p>
-<div class="icon-container">
-    <div class="icon icon-fill">
+<class="icon-container">
+    <button class="icon-button icon-fill" onclick="location.href='/'">
         <i class="fa fa-home"></i>
-    </div>
-    <p></p>
-    <div class="icon icon-enter">
+    </button>
+    
+    <button class="icon-button icon-enter" onclick="location.href='/perfil'">
         <i class="fa fa-user"></i>
-    </div>
-    <p></p>
-    <div class="icon icon-expand">
-        <a href="/reserva_prof">
-            <i class="fa fa-bars"></i>
-        </a>
-    </div>
-    <p></p>
-    <div class="icon icon-collapse">
+    </button>
+    
+    <button class="icon-button icon-expand" onclick="location.href='/reserva_prof'">
+        <i class="fa fa-bars"></i>
+    </button>
+    
+    <button class="icon-button icon-collapse" onclick="location.href='/login'">
         <i class="fa fa-sign-in"></i>
-    </div>
-    <p></p>
-    <div class="icon icon-rotate">
+    </button>
+    
+    <button class="icon-button icon-rotate" onclick="location.href='/contato'">
         <i class="fa fa-phone"></i>
-    </div>
-</div>
-</i>
+    </button>
 </div>
 </div>
 </div>
 </body>
 </html>
-<body class="center">
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<h2>
