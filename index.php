e06a4991809d2f59fc8ba97362d606a7f36b2d99
Tomás Almeida <a9992@aejics.org>
2025-03-26 11:45:07 +0000

    update

--- index.php
+++ index.php
@@ -11,16 +11,18 @@
         <a href=""><img src="https://avatars.githubusercontent.com/u/181112077?s=200&v=4" class="logo"></a>
         <div class="list">
             <ul>
-                <li><a href="">Login</a></li>
-                <li><a href="">Perfil</a></li>
-                <li><a href="">Suporte</a></li>
+                <li><a href="/iniciar_sessao">Login</a></li>
+                <li><a href="/perfil">Perfil</a></li>
+                <li><a href="/suporte">Suporte</a></li>
             </ul> 
         </div>
     </nav>
     <div class="text">
         <h2>BEM VINDO à<br> <span>Reserva De Salas</span> </h2>
         <p>feito pela turma 2ºE e 1ºD 2024/25</p>
-        <button class="btn">Reservar sala</button>
+        <button  class="btn">Reservar sala
+        <a href="/reserva_prof"></a>
+        </button>
     </div>
     <img src="coloca a imagem" alt="" class="img">
     <div class="circle"></div>
